71dc49c75dfbb8625661027cf8b7a7b71e9ca7f2
shubhada10kunde <shubhadakunde8@gmail.com>
2024-06-05 00:11:00 +0530

    pattern 2

---
+++ Patterns/pattern2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int r = 0; r < 5; r++)
+    {
+        for (int c = 0; c < 4; c++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
